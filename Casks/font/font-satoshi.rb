b683b3301f2929f259a98793e62f27b101a7b7bb
Pogimon3 <140201901+Pogimon3@users.noreply.github.com>
2024-08-09 14:36:52 +0530

    Update font-satoshi.rb

--- Casks/font/font-satoshi.rb
+++ Casks/font/font-satoshi.rb
@@ -1,7 +1,7 @@
 cask "font-satoshi" do
   version "1.0"
-  sha256 :no_check 
-
+  sha256 "ec29c9d6ff5b58b200d00b545884312cd621f83608b0806cfca3461ce4926a43"
+  
   depends_on "curl"
 
   url "https://api.fontshare.com/v2/fonts/download/satoshi", using: :homebrew_curl
@@ -9,16 +9,16 @@ cask "font-satoshi" do
   homepage "https://www.fontshare.com/fonts/satoshi"
 
   font "/Fonts/OTF/Satoshi-BlackItalic.otf"
-  #font "/Fonts/OTF/Satoshi-Bold.otf"
-  #font "/Fonts/OTF/Satoshi-BoldItalic.otf"
-  #font "/Fonts/OTF/Satoshi-Italic.otf"
-  #font "/Fonts/OTF/Satoshi-Light.otf"
-  #font "/Fonts/OTF/Satoshi-LightItalic.otf"
-  #font "/Fonts/OTF/Satoshi-Medium.otf"
-  #font "/Fonts/OTF/Satoshi-MediumItalic.otf"
-  #font "/Fonts/OTF/Satoshi-Regular.otf"
-  #font "/Fonts/TTF/Satoshi-Variable.ttf"
-  #font "/Fonts/TTF/Satoshi-VariableItalic.ttf"
+  font "/Fonts/OTF/Satoshi-Bold.otf"
+  font "/Fonts/OTF/Satoshi-BoldItalic.otf"
+  font "/Fonts/OTF/Satoshi-Italic.otf"
+  font "/Fonts/OTF/Satoshi-Light.otf"
+  font "/Fonts/OTF/Satoshi-LightItalic.otf"
+  font "/Fonts/OTF/Satoshi-Medium.otf"
+  font "/Fonts/OTF/Satoshi-MediumItalic.otf"
+  font "/Fonts/OTF/Satoshi-Regular.otf"
+  font "/Fonts/TTF/Satoshi-Variable.ttf"
+  font "/Fonts/TTF/Satoshi-VariableItalic.ttf"
 
     # No zap stanza required
 end
