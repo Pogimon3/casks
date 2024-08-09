60b27315bdf120c2e0cbba1047a45493feb543cb
Pogimon3 <140201901+Pogimon3@users.noreply.github.com>
2024-08-09 15:19:55 +0530

    Update font-satoshi.rb

--- Casks/font/font-satoshi.rb
+++ Casks/font/font-satoshi.rb
@@ -1,25 +1,30 @@
 cask "font-satoshi" do
   version "1.0"
 
-  url "https://api.fontshare.com/v2/fonts/download/satoshi",
-      using:      :post,
-      verified:   "api.fontshare.com", # Add verification for security
-      binary:     "satoshi-v#{version}.zip" # Specify the desired filename
-  
+  url "https://api.fontshare.com/v2/fonts/download/satoshi", using: :post
   name "Satoshi"
   homepage "https://www.fontshare.com/fonts/satoshi"
 
-  font "/OTF/Satoshi-BlackItalic.otf"
-  font "/Fonts/OTF/Satoshi-Bold.otf"
-  font "/Fonts/OTF/Satoshi-BoldItalic.otf"
-  font "/Fonts/OTF/Satoshi-Italic.otf"
-  font "/Fonts/OTF/Satoshi-Light.otf"
-  font "/Fonts/OTF/Satoshi-LightItalic.otf"
-  font "/Fonts/OTF/Satoshi-Medium.otf"
-  font "/Fonts/OTF/Satoshi-MediumItalic.otf"
-  font "/Fonts/OTF/Satoshi-Regular.otf"
-  font "/Fonts/TTF/Satoshi-Variable.ttf"
-  font "/Fonts/TTF/Satoshi-VariableItalic.ttf"
+  postflight do
+    downloaded_file = "#{staged_path}/satoshi" # Assuming it's downloaded as 'satoshi'
+    target_file = "#{staged_path}/satoshi.zip" # Renaming to 'satoshi.zip'
 
-    # No zap stanza required
+    File.rename(downloaded_file, target_file)
+
+    system_command "unzip", args: ["-o", target_file, "-d", "#{staged_path}"]
+  end
+
+  font "#{staged_path}/OTF/Satoshi-BlackItalic.otf"
+  font "#{staged_path}/OTF/Satoshi-Bold.otf"
+  font "#{staged_path}/OTF/Satoshi-BoldItalic.otf"
+  font "#{staged_path}/OTF/Satoshi-Italic.otf"
+  font "#{staged_path}/OTF/Satoshi-Light.otf"
+  font "#{staged_path}/OTF/Satoshi-LightItalic.otf"
+  font "#{staged_path}/OTF/Satoshi-Medium.otf"
+  font "#{staged_path}/OTF/Satoshi-MediumItalic.otf"
+  font "#{staged_path}/OTF/Satoshi-Regular.otf"
+  font "#{staged_path}/TTF/Satoshi-Variable.ttf"
+  font "#{staged_path}/TTF/Satoshi-VariableItalic.ttf"
+
+  # No zap stanza required
 end
