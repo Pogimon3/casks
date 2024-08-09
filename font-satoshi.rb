22735761f4d9c07b1309dc95be5fb36e35743c9c
Pogimon3 <140201901+Pogimon3@users.noreply.github.com>
2024-08-09 13:42:05 +0530

    Create font-satoshi.rb

---
+++ font-satoshi.rb
@@ -0,0 +1,23 @@
+cask "font-satoshi" do
+  version "1.0"
+  sha256 "ec29c9d6ff5b58b200d00b545884312cd621f83608b0806cfca3461ce4926a43"
+
+  url "https://api.fontshare.com/v2/fonts/download/satoshi"
+  name "Satoshi"
+  homepage "https://www.fontshare.com/fonts/satoshi"
+
+  font /Fonts/OTF/Satoshi-Black.otf
+  font /Fonts/OTF/Satoshi-BlackItalic.otf
+  font /Fonts/OTF/Satoshi-Bold.otf
+  font /Fonts/OTF/Satoshi-BoldItalic.otf
+  font /Fonts/OTF/Satoshi-Italic.otf
+  font /Fonts/OTF/Satoshi-Light.otf
+  font /Fonts/OTF/Satoshi-LightItalic.otf
+  font /Fonts/OTF/Satoshi-Medium.otf
+  font /Fonts/OTF/Satoshi-MediumItalic.otf
+  font /Fonts/OTF/Satoshi-Regular.otf
+  font /Fonts/TTF/Satoshi-Variable.ttf
+  font /Fonts/TTF/Satoshi-VariableItalic.ttf
+
+    # No zap stanza required
+end
