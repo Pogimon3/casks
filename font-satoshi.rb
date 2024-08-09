37d4529ee5212e21994be2700522044d9bd71469
Pogimon3 <140201901+Pogimon3@users.noreply.github.com>
2024-08-09 13:50:26 +0530

    add missing quotes

--- font-satoshi.rb
+++ font-satoshi.rb
@@ -6,18 +6,17 @@ cask "font-satoshi" do
   name "Satoshi"
   homepage "https://www.fontshare.com/fonts/satoshi"
 
-  font /Fonts/OTF/Satoshi-Black.otf
-  font /Fonts/OTF/Satoshi-BlackItalic.otf
-  font /Fonts/OTF/Satoshi-Bold.otf
-  font /Fonts/OTF/Satoshi-BoldItalic.otf
-  font /Fonts/OTF/Satoshi-Italic.otf
-  font /Fonts/OTF/Satoshi-Light.otf
-  font /Fonts/OTF/Satoshi-LightItalic.otf
-  font /Fonts/OTF/Satoshi-Medium.otf
-  font /Fonts/OTF/Satoshi-MediumItalic.otf
-  font /Fonts/OTF/Satoshi-Regular.otf
-  font /Fonts/TTF/Satoshi-Variable.ttf
-  font /Fonts/TTF/Satoshi-VariableItalic.ttf
+  font "/Fonts/OTF/Satoshi-BlackItalic.otf"
+  font "/Fonts/OTF/Satoshi-Bold.otf"
+  font "/Fonts/OTF/Satoshi-BoldItalic.otf"
+  font "/Fonts/OTF/Satoshi-Italic.otf"
+  font "/Fonts/OTF/Satoshi-Light.otf"
+  font "/Fonts/OTF/Satoshi-LightItalic.otf"
+  font "/Fonts/OTF/Satoshi-Medium.otf"
+  font "/Fonts/OTF/Satoshi-MediumItalic.otf"
+  font "/Fonts/OTF/Satoshi-Regular.otf"
+  font "/Fonts/TTF/Satoshi-Variable.ttf"
+  font "/Fonts/TTF/Satoshi-VariableItalic.ttf"
 
     # No zap stanza required
 end
