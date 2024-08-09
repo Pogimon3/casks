330709efd32b5f16c77331d58403b7e60c578030
Pogimon3 <140201901+Pogimon3@users.noreply.github.com>
2024-08-09 15:18:28 +0530

    Update font-satoshi.rb

--- Casks/font/font-satoshi.rb
+++ Casks/font/font-satoshi.rb
@@ -1,25 +1,25 @@
 cask "font-satoshi" do
   version "1.0"
 
-  #depends_on "curl" => :build
-
-  url "https://api.fontshare.com/v2/fonts/download/satoshi", using: :post
+  url "https://api.fontshare.com/v2/fonts/download/satoshi",
+      using:      :post,
+      verified:   "api.fontshare.com", # Add verification for security
+      binary:     "satoshi-v#{version}.zip" # Specify the desired filename
+  
   name "Satoshi"
   homepage "https://www.fontshare.com/fonts/satoshi"
 
-  container type: :zip
-
   font "/OTF/Satoshi-BlackItalic.otf"
-  #font "/Fonts/OTF/Satoshi-Bold.otf"
-  #font "/Fonts/OTF/Satoshi-BoldItalic.otf"
-  #font "/Fonts/OTF/Satoshi-Italic.otf"
-  #font "/Fonts/OTF/Satoshi-Light.otf"
-  #font "/Fonts/OTF/Satoshi-LightItalic.otf"
-  #font "/Fonts/OTF/Satoshi-Medium.otf"
-  #font "/Fonts/OTF/Satoshi-MediumItalic.otf"
-  #font "/Fonts/OTF/Satoshi-Regular.otf"
-  #font "/Fonts/TTF/Satoshi-Variable.ttf"
-  #font "/Fonts/TTF/Satoshi-VariableItalic.ttf"
+  font "/Fonts/OTF/Satoshi-Bold.otf"
+  font "/Fonts/OTF/Satoshi-BoldItalic.otf"
+  font "/Fonts/OTF/Satoshi-Italic.otf"
+  font "/Fonts/OTF/Satoshi-Light.otf"
+  font "/Fonts/OTF/Satoshi-LightItalic.otf"
+  font "/Fonts/OTF/Satoshi-Medium.otf"
+  font "/Fonts/OTF/Satoshi-MediumItalic.otf"
+  font "/Fonts/OTF/Satoshi-Regular.otf"
+  font "/Fonts/TTF/Satoshi-Variable.ttf"
+  font "/Fonts/TTF/Satoshi-VariableItalic.ttf"
 
     # No zap stanza required
 end
