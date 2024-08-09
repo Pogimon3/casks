1fc9fc147677c792de9bc334c8247bce5fc6c381
Pogimon3 <140201901+Pogimon3@users.noreply.github.com>
2024-08-09 14:13:56 +0530

    Update font-satoshi.rb

--- Casks/font/font-satoshi.rb
+++ Casks/font/font-satoshi.rb
@@ -13,15 +13,15 @@ cask "font-satoshi" do
                    args: ["-d", staged_path.to_s, "#{staged_path}/satoshi.zip"]
   end
 
-  font "/Fonts/OTF/Satoshi-BlackItalic.otf"
-  font "/Fonts/OTF/Satoshi-Bold.otf"
-  font "/Fonts/OTF/Satoshi-BoldItalic.otf"
-  font "/Fonts/OTF/Satoshi-Italic.otf"
-  font "/Fonts/OTF/Satoshi-Light.otf"
-  font "/Fonts/OTF/Satoshi-LightItalic.otf"
-  font "/Fonts/OTF/Satoshi-Medium.otf"
-  font "/Fonts/OTF/Satoshi-MediumItalic.otf"
-  font "/Fonts/OTF/Satoshi-Regular.otf"
+  #font "/Fonts/OTF/Satoshi-BlackItalic.otf"
+  #font "/Fonts/OTF/Satoshi-Bold.otf"
+  #font "/Fonts/OTF/Satoshi-BoldItalic.otf"
+  #font "/Fonts/OTF/Satoshi-Italic.otf"
+  #font "/Fonts/OTF/Satoshi-Light.otf"
+  #font "/Fonts/OTF/Satoshi-LightItalic.otf"
+  #font "/Fonts/OTF/Satoshi-Medium.otf"
+  #font "/Fonts/OTF/Satoshi-MediumItalic.otf"
+  #font "/Fonts/OTF/Satoshi-Regular.otf"
   #font "/Fonts/TTF/Satoshi-Variable.ttf"
   #font "/Fonts/TTF/Satoshi-VariableItalic.ttf"
 
