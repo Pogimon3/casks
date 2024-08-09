a0f46cc1e940e21e220e87912b7cf5366f3930a5
Pogimon3 <140201901+Pogimon3@users.noreply.github.com>
2024-08-09 15:23:15 +0530

    Update font-satoshi.rb

--- Casks/font/font-satoshi.rb
+++ Casks/font/font-satoshi.rb
@@ -15,16 +15,7 @@ cask "font-satoshi" do
   end
 
   font "#{staged_path}/Satoshi_Complete/OTF/Satoshi-BlackItalic.otf"
-  font "#{staged_path}/OTF/Satoshi-Bold.otf"
-  font "#{staged_path}/OTF/Satoshi-BoldItalic.otf"
-  font "#{staged_path}/OTF/Satoshi-Italic.otf"
-  font "#{staged_path}/OTF/Satoshi-Light.otf"
-  font "#{staged_path}/OTF/Satoshi-LightItalic.otf"
-  font "#{staged_path}/OTF/Satoshi-Medium.otf"
-  font "#{staged_path}/OTF/Satoshi-MediumItalic.otf"
-  font "#{staged_path}/OTF/Satoshi-Regular.otf"
-  font "#{staged_path}/TTF/Satoshi-Variable.ttf"
-  font "#{staged_path}/TTF/Satoshi-VariableItalic.ttf"
+  
 
   # No zap stanza required
 end
